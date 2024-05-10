7254461d340534f1c509ef74c95faef6c05851f9
Adam Halnon <halnon_01@hotmail.com>
2024-05-10 15:10:06 +0100

    refactor class name val and add earth icon

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import './NavBar.module.css';
+import styles from './NavBar.module.css';
 
 export default function NavBar() {
     return (
-        <nav className="nav--bar">
+        <nav className={styles.nav_bar}>
             <i class="fa-solid fa-earth-europe"></i>
             <h2>Adam's travel journal</h2>
         </nav>
