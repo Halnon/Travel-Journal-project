cdba446916b170086855324d6fd69a20962db277
Adam Halnon <halnon_01@hotmail.com>
2024-05-10 15:26:02 +0100

    remove fontawesome icon as need to install package first

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,6 @@ import styles from './NavBar.module.css';
 export default function NavBar() {
     return (
         <nav className={styles.nav_bar}>
-            <i class="fa-solid fa-earth-europe"></i>
             <h2>Adam's travel journal</h2>
         </nav>
     )
