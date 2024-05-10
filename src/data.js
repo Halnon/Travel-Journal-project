e211449b3106a2928e36f7160e0a8e9309e6d534
Adam Halnon <halnon_01@hotmail.com>
2024-05-10 17:39:47 +0100

    initial commit

---
+++ src/data.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        id: 1,
+        coverImg: '',
+        country: '',
+        location: '',
+        dates: '',
+        description: ''
+    },
+    {
+        id: 2,
+        coverImg: '',
+        country: '',
+        location: '',
+        dates: '',
+        description: ''
+    },
+    {
+        id: 3,
+        coverImg: '',
+        country: '',
+        location: '',
+        dates: '',
+        description: ''
+    }
+]
