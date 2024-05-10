06538143873aec37bee65b18c50490f024acc420
Adam Halnon <halnon_01@hotmail.com>
2024-05-10 16:31:09 +0100

    import and render fontawesome earth icon and set class name

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import styles from './NavBar.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEarthEurope } from '@fortawesome/free-solid-svg-icons'
+
 
 export default function NavBar() {
     return (
         <nav className={styles.nav_bar}>
+            <FontAwesomeIcon icon={faEarthEurope} className={styles.cd} />
             <h2>Adam's travel journal</h2>
         </nav>
     )
