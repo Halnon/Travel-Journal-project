4a15edb5913747bff51ef16386e66d5f044e4265
Adam Halnon <halnon_01@hotmail.com>
2024-05-10 17:45:28 +0100

    add initial setup for props and comments and explain how I want components to render

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import styles from './Card.module.css';
 
-export default function Card() {
+export default function Card(props) {
     return (
         <div className={styles.card_container}>
-            <p>hello</p>
+            <div className={styles.large_container}>
+                <img src={'#'} alt="main location" />
+                <h1>{}</h1>
+
+            </div>
+
+            <div className={styles.small_container}>
+
+            </div>
         </div>
     )
 }
+
+//card container should provide entire container to fit cards in
+//large container will show card from list of data objects on left side of container larger then adjacent smaller cards
+//small container will show only smaller images of other cards yet to be clicked on 
