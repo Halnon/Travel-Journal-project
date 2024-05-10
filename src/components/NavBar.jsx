66d8389c8a17e6a7fc977d68cefdafcdb4559137
Adam Halnon <halnon_01@hotmail.com>
2024-05-10 16:39:25 +0100

    fix typo w/ icon class name

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,7 +7,7 @@ import { faEarthEurope } from '@fortawesome/free-solid-svg-icons'
 export default function NavBar() {
     return (
         <nav className={styles.nav_bar}>
-            <FontAwesomeIcon icon={faEarthEurope} className={styles.cd} />
+            <FontAwesomeIcon icon={faEarthEurope} className={styles.earth_icon} />
             <h2>Adam's travel journal</h2>
         </nav>
     )
