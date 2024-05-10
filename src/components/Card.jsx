dfe82128cea54588152abf1ad4cf14c1e6e08536
Adam Halnon <halnon_01@hotmail.com>
2024-05-10 17:12:47 +0100

    import styles and add class name to component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import styles from './Card.module.css';
 
 export default function Card() {
     return (
-        <div>
-            
+        <div className={styles.card_container}>
+            <p>hello</p>
         </div>
     )
 }
