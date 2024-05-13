68ee6dd26a136ef51600598041bba03ab33d57d9
Adam Halnon <halnon_01@hotmail.com>
2024-05-13 21:30:04 +0100

    add data for eze village

--- src/data.js
+++ src/data.js
@@ -10,22 +10,22 @@ export default [
     },
     {
         id: 2,
-        coverImg: '',
-        country: '',
-        mapsLink: '',
-        location: '',
-        dates: '',
-        description: ''
+        coverImg: 'eze.jpg',
+        country: 'France',
+        mapsLink: "https://www.google.com/maps/place/06360+%C3%88ze,+France/@43.7298928,7.3153591,13z/data=!3m1!4b1!4m6!3m5!1s0x12cdc36149af4541:0x40819a5fd979c40!8m2!3d43.7278189!4d7.361792!16zL20vMDkybXl0?entry=ttu",
+        location: 'Eze Village',
+        dates: '23 July 2023 - 3 August 2023',
+        description: "Èze is a seaside commune in the Alpes-Maritimes department in the Provence-Alpes-Côte d'Azur region in Southeastern France. It is located on the French Riviera, 8.5 km to the northeast of Nice and 4.5 km to the west of Monaco. In 2018, Èze had a population of 2,225. Its inhabitants are known as Ezasques."
     },
-    {
-        id: 3,
-        coverImg: '',
-        country: '',
-        mapsLink: '',
-        location: '',
-        dates: '',
-        description: ''
-    }
+    // {
+    //     id: 3,
+    //     coverImg: '',
+    //     country: '',
+    //     mapsLink: "",
+    //     location: '',
+    //     dates: '',
+    //     description: ""
+    // }
 ]
 
 //template below
@@ -34,8 +34,8 @@ export default [
 id: 1,
 coverImg: '',
 country: '',
-mapsLink: '',
+mapsLink: "",
 location: '',
 dates: '',
-description: ''
+description: ""
 */
