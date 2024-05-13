093bb0f059ad1853f924b1daf8cd6e1ab98937d4
Adam Halnon <halnon_01@hotmail.com>
2024-05-13 19:49:20 +0100

    add first set of data

--- src/data.js
+++ src/data.js
@@ -1,12 +1,12 @@
 export default [
     {
         id: 1,
-        coverImg: '',
-        country: '',
-        mapsLink: '',
-        location: '',
-        dates: '',
-        description: ''
+        coverImg: 'pisa.jpg',
+        country: 'Italy',
+        mapsLink: 'https://www.google.com/maps/place/Pisa,+Province+of+Pisa,+Italy/@43.7067507,10.3953781,13z/data=!3m1!4b1!4m6!3m5!1s0x12d5919af0f6598f:0xaab80fb5a78478c8!8m2!3d43.7228385!4d10.4017581!16zL20vMDY0eHA?entry=ttu',
+        location: 'Pisa',
+        dates: '11 April 2018 - 18 April 2018',
+        description: "Pisa is a city in Italy's Tuscany region best known for its iconic Leaning Tower. Already tilting when it was completed in 1372, the 56m white-marble cylinder is the bell tower of the Romanesque, striped-marble cathedral that rises next to it in the Piazza dei Miracoli. Also in the piazza is the Baptistry, whose renowned acoustics are demonstrated by amateur singers daily, and the Caposanto Monumentale cemetery."
     },
     {
         id: 2,
